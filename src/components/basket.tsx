d5eacef026d1bb72ca88fbfed4b51f383a2e3375
Fatih20 <fatih@ThinkPad-X230-1b1323db.localdomain>
2022-01-27 10:06:59 +0700

    Styling of the basket page.

--- src/components/basket.tsx
+++ src/components/basket.tsx
@@ -7,17 +7,32 @@ import useAddToCartIfLoggedIn from '../utils/addToCartIfLoggedIn';
 import { deleteFromCart } from '../utils/api';
 
 import { useMutation, useQueryClient } from 'react-query';
+import { ICartItem } from '../types';
+
+interface ICartItemProps {
+  uniqueItemName: string;
+  initialAmount: number;
+  // setAmount: () => void;
+}
 
 const Main = styled.div`
   display: flex;
   flex-direction: column;
+  margin: 0 auto;
+  max-width: 300px;
   padding: 1rem;
+  width: 100%;
+
+  @media (min-width: 600px) {
+    max-width: 450px;
+  }
 `;
 
 const Title = styled.h2`
   color: #8c7466;
   font-weight: 900;
   font-size: 1.5em;
+  margin-bottom: 0.5rem;
   text-align: center;
 
   @media (min-width: 600px) {
@@ -28,11 +43,83 @@ const Title = styled.h2`
 const ActualBasket = styled.div`
   display: flex;
   flex-direction: column;
+  gap: 1.5rem;
+  margin-bottom: 1rem;
 `;
 
-const BasketItem = styled.div`
+const BasketItemMain = styled.div`
+  align-items: center;
+  background-color: #fff;
+  color: #8c7466;
+  border: solid 2px #8c7466;
+  border-radius: 0.1rem;
   display: flex;
+  flex-direction: column;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+  gap: 0.5rem;
+  padding: 0.5rem;
+
+  @media (min-width: 600px) {
+    flex-direction: row;
+  }
 `;
+
+const BasketItemTitle = styled.h3`
+  font-size: 1.25rem;
+  font-weight: bold;
+`;
+
+const AmountContainer = styled.div`
+  display: flex;
+  gap: 0.75rem;
+  justify-content: center;
+`;
+
+const AmountField = styled.input`
+  background-color: rgba(0, 0, 0, 0);
+  text-align: center;
+  width: 3rem;
+
+  /* border: solid 1px black; */
+`;
+
+const AddRemoveButton = styled.button`
+  width: 1rem;
+  border-radius: 0.1rem;
+`;
+
+const CheckOutButton = styled.button`
+  background-color: #95c79d;
+  border-radius: 0.1rem;
+  color: white;
+  font-size: 1em;
+  font-weight: 900;
+  margin: 0.5rem 0 0 0;
+  padding: 0.3rem 0.75rem;
+`;
+
+const Spacer = styled.div`
+  flex-grow: 0;
+
+  @media (min-width: 600px) {
+    flex-grow: 1;
+  }
+`;
+
+function CartItem({ uniqueItemName, initialAmount }: ICartItemProps) {
+  const [value, setValue] = useState(initialAmount);
+  return (
+    <BasketItemMain>
+      <BasketItemTitle>{uniqueItemName}</BasketItemTitle>
+      <Spacer />
+      <AmountContainer>
+        <AddRemoveButton>-</AddRemoveButton>
+        <AmountField value={value} />
+        <AddRemoveButton>+</AddRemoveButton>
+      </AmountContainer>
+    </BasketItemMain>
+  );
+}
 
 export default function Basket() {
   const navigate = useNavigate();
@@ -117,34 +204,43 @@ export default function Basket() {
               uniqueItemNameAndId.split('|||');
             // const eachItemsOfTheName = groupedItems[uniqueItemName];
             return (
-              <BasketItem>
-                <h3>{uniqueItemName}</h3>
-                <button
-                  onClick={() => {
-                    deleteFromBasketAndUpdateCart(
-                      groupedItems[uniqueItemName][
-                        groupedItems[uniqueItemName].length - 1
-                      ].id
-                    );
-                    regroupItems();
-                  }}
-                >
-                  minus
-                </button>
-                <p>
-                  {
-                    cart.filter((menuItem) => {
-                      return menuItem.menu.name === uniqueItemName;
-                    }).length
-                  }
-                </p>
-                <button onClick={() => addToBasketAndUpdateCart(uniqueItemId)}>
-                  plus
-                </button>
-              </BasketItem>
+              <CartItem
+                uniqueItemName={uniqueItemName}
+                initialAmount={
+                  cart.filter(
+                    (menuItem) => menuItem.menu.name === uniqueItemName
+                  ).length
+                }
+              />
+              // <BasketItem>
+              //   <h3>{uniqueItemName}</h3>
+              //   <button
+              //     onClick={() => {
+              //       deleteFromBasketAndUpdateCart(
+              //         groupedItems[uniqueItemName][
+              //           groupedItems[uniqueItemName].length - 1
+              //         ].id
+              //       );
+              //       regroupItems();
+              //     }}
+              //   >
+              //     minus
+              //   </button>
+              //   <p>
+              //     {
+              //       cart.filter((menuItem) => {
+              //         return menuItem.menu.name === uniqueItemName;
+              //       }).length
+              //     }
+              //   </p>
+              //   <button onClick={() => addToBasketAndUpdateCart(uniqueItemId)}>
+              //     plus
+              //   </button>
+              // </BasketItem>
             );
           })}
         </ActualBasket>
+        <CheckOutButton>Go To Checkout</CheckOutButton>
       </Main>
     );
   }
