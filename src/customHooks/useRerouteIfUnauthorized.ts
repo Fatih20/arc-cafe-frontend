6a9ab29f68e93945291f04372c6b1aa8754e55c4
Fatih20 <fatih@ThinkPad-X230-1b1323db.localdomain>
2022-01-29 14:43:39 +0700

    A little clean up.

--- src/customHooks/useRerouteIfUnauthorized.ts
+++ src/customHooks/useRerouteIfUnauthorized.ts
@@ -8,11 +8,11 @@ export default function useRerouteIfUnauthorized (){
     const navigate = useNavigate();
 
     useEffect(() => {
-        if (!isLoading){
-            console.log(`The user is ${user}`)
-            console.log(`The error is ${error}`)
-        }
-        console.log(error)
+        // if (!isLoading){
+        //     console.log(`The user is ${user}`)
+        //     console.log(`The error is ${error}`)
+        // }
+        // console.log(error)
         if (error !== null){
             navigate(`${BASE_URL}/signup`);
         }
